1b4cf4b43106a54dd9b588f6a6b71d3ecdc0a32a
LearnJS <learnjs@MacBook-Pro-Egor.local>
2025-10-20 23:10:43 +0300

    feat: add load data by scroll

--- src/app/pages/products-list/products-list.ts
+++ src/app/pages/products-list/products-list.ts
@@ -3,10 +3,11 @@ import {Card} from './card/card';
 import {productsMock} from '../../shared/products/products.mock';
 import {Product} from '../../shared/products/product.type';
 import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {ScrollWithLoadingDirective} from '../../shared/scroll-with-loading/scroll-with-loading.directive';
 
 @Component({
     selector: 'app-products-list',
-    imports: [Card, MatProgressSpinner],
+    imports: [Card, MatProgressSpinner, ScrollWithLoadingDirective],
     templateUrl: './products-list.html',
     styleUrl: './products-list.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,6 +17,10 @@ export class ProductsList {
 
     constructor() {
         this.loadProducts();
+    }
+
+    protected loadNextData() {
+        console.log('Load next data');
     }
 
     private loadProducts() {
---
+++ src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
@@ -0,0 +1,35 @@
+import {Directive, ElementRef, inject, output} from '@angular/core';
+import {isScrollReachedBottomOffcet} from './utils/is-scroll-reached-bottom-offcet';
+
+@Directive({
+    selector: '[appScrollWithLoading]',
+    standalone: true,
+    host: {
+        '(scroll)': 'onScroll()',
+    },
+})
+export class ScrollWithLoadingDirective {
+    private readonly element = inject<ElementRef<HTMLElement>>(ElementRef).nativeElement;
+
+    private prevScrollTop = -1;
+
+    readonly loadNextData = output<void>();
+
+    onScroll() {
+        const {scrollTop, clientHeight, scrollHeight} = this.element;
+        const prevScrollTop = this.prevScrollTop;
+
+        this.prevScrollTop = scrollTop;
+
+        const lowerScrollPosition = scrollHeight - clientHeight;
+        const shouldLoadMessagesDown = isScrollReachedBottomOffcet(
+            scrollTop,
+            lowerScrollPosition,
+            prevScrollTop,
+        );
+
+        if (shouldLoadMessagesDown) {
+            this.loadNextData.emit();
+        }
+    }
+}
