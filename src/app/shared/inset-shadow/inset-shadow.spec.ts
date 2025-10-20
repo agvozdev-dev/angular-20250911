e3fa58f5beb9ab2ce288b2ed541b637a6bdc7eae
LearnJS <learnjs@MacBook-Pro-Egor.local>
2025-10-20 22:50:20 +0300

    feat: lecture 8

---
+++ src/app/shared/inset-shadow/inset-shadow.spec.ts
@@ -0,0 +1,8 @@
+import {InsetShadow} from './inset-shadow';
+
+describe('InsetShadow', () => {
+    it('should create an instance', () => {
+        const directive = new InsetShadow();
+        expect(directive).toBeTruthy();
+    });
+});
---
+++ src/app/shared/inset-shadow/inset-shadow.ts
@@ -0,0 +1,17 @@
+import {Directive, ElementRef, inject, signal} from '@angular/core';
+
+@Directive({
+    selector: '[appInsetShadow]',
+    host: {
+        '[style.boxShadow]': 'shadow()',
+        '(click)': 'onClick($event)',
+    },
+})
+export class InsetShadow {
+    protected readonly shadow = signal('');
+    // private readonly element = inject(ElementRef).nativeElement;
+
+    protected onClick(_event: Event) {
+        this.shadow.update(currentShadow => (currentShadow ? '' : 'inset 0 0 10px #000'));
+    }
+}
