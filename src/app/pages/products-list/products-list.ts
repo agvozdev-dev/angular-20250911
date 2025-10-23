e6d400e720984e96f252e33946ae67dc6b40e499
LearnJS <learnjs@MacBook-Pro-Egor.local>
2025-10-23 22:54:52 +0300

    feat: add filter by property pipe

--- src/app/pages/products-list/products-list.ts
+++ src/app/pages/products-list/products-list.ts
@@ -4,10 +4,11 @@ import {Product} from '../../shared/products/product.type';
 import {MatProgressSpinner} from '@angular/material/progress-spinner';
 import {ScrollWithLoadingDirective} from '../../shared/scroll-with-loading/scroll-with-loading.directive';
 import {ProductsStoreService} from '../../shared/products/products-store.service';
+import {FilterByPropertyPipe} from '../../shared/filter-by-property/filter-by-property-pipe';
 
 @Component({
     selector: 'app-products-list',
-    imports: [Card, MatProgressSpinner, ScrollWithLoadingDirective],
+    imports: [Card, MatProgressSpinner, ScrollWithLoadingDirective, FilterByPropertyPipe],
     templateUrl: './products-list.html',
     styleUrl: './products-list.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
