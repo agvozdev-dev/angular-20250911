2d59c5248ee1b0568dcd161e3c4798aaae5aa5b6
agvozdev <alex@calipso.app>
2025-11-20 19:04:11 +0700

    доработка header

--- src/app/core-components/header/header.ts
+++ src/app/core-components/header/header.ts
@@ -2,13 +2,20 @@ import {Component} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
     selector: 'app-header',
-    imports: [MatToolbar, MatIcon, MatIconButton],
+    imports: [MatToolbar, MatIcon, MatIconButton, NgOptimizedImage],
     templateUrl: './header.html',
     styleUrl: './header.css',
 })
 export class Header {
     protected readonly title = 'Header';
+    protected readonly imgUrl = './favicon.ico';
+    protected readonly onclick = onclick;
+
+    protected menuClick($event: PointerEvent) {
+        console.log('menu clicked');
+    }
 }
